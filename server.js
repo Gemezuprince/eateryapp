eab2ac6694c02178cf25c1c561fde9b4f9ed5189
Gemezuprince <gemezuprince1999@gmail.com>
2026-07-11 19:37:55 +0100

    Fix payment issues

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://eatery-xi.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
